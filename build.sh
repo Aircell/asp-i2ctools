9af04b5f379f7127bf84ae50beeaeb65908630f6
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-04-18 14:39:58 -0600

    Use relative paths to find agcc.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#/bin/sh
+#/bin/bash -eu
 # Cross-compile for Android
 #
-CS=/mnt/build/cloudsurfer
-export PATH=$PATH:$CS/cs-android-kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin:$CS/cs-android-agcc
+
+: ${workspace:=..}
+PATH+=:$workspace/cs-android-kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin:$workspace/cs-android-agcc
 export CC=agcc
-make 
+make
