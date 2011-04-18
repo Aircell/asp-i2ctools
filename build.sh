ca9fa84f1c0d0abc9f33d6d05db70aac8bb0eb38
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-04-18 16:58:09 -0600

    Add an installation step.
    Stick built executables into the RFS.

--- build.sh
+++ build.sh
@@ -3,6 +3,23 @@
 #
 
 : ${workspace:=..}
+
+# build step
 PATH+=:$workspace/cs-android-kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin:$workspace/cs-android-agcc
 export CC=agcc
 make
+
+# installation step
+installdir=$workspace/..
+mkdir -p /tmp/system/bin
+cd tools
+cp i2cdetect i2cdump i2cget i2cset /tmp/system/bin
+cd /tmp
+
+# unpack the root file system, stick in the built files, then re-pack it.
+bunzip2 $installdir/rootfs.tar.bz2
+new_uid=0
+new_gid=2000
+tar --numeric-owner --owner $new_uid --group $new_gid -rf $installdir/rootfs.tar system
+bzip2 $installdir/rootfs.tar
+rm -rf /tmp/system
