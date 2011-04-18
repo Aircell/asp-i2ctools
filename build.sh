27503e0bddded5f9004a142d59fe0c0b9cd63237
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-04-18 17:27:06 -0600

    absolute paths

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Cross-compile for Android
 #
 
-: ${workspace:=..}
+: ${workspace:=$PWD/..}
 
 # build step
 PATH+=:$workspace/cs-android-kernel/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin:$workspace/cs-android-agcc
