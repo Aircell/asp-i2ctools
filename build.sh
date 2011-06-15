5358654e4fc9f906930de4d439d0f07def311eb1
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2011-06-15 12:13:10 -0600

    read variable settings

--- build.sh
+++ build.sh
@@ -2,24 +2,12 @@
 # Cross-compile for Android
 #
 
-source ../bin/bash-android
+[ -f ../bin/bash-android ] && source ../bin/bash-android
+[ -f ~/.bash-android ] && source ~/.bash-android
+
 export CC=agcc
 make
 make install
+cp local/sbin/* $STAGING_AREA/system/app
 
 exit 0
-
-# installation step, needs to be moved into Hudson
-installdir=$workspace/../install.d
-mkdir -p /tmp/system/bin
-cd tools
-cp i2cdetect i2cdump i2cget i2cset /tmp/system/bin
-cd /tmp
-
-# unpack the root file system, stick in the built files, then re-pack it.
-bunzip2 $installdir/rootfs.tar.bz2
-new_uid=0
-new_gid=2000
-tar --numeric-owner --owner $new_uid --group $new_gid -rf $installdir/rootfs.tar system
-bzip2 $installdir/rootfs.tar
-rm -rf /tmp/system
